80b6798ac6dd789323ddcc30f02a80654264844c
HAZEM <hazemabdu2n@gmail.com>
2023-06-24 21:15:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn-dark');
+const bg = document.querySelector('.calculator')
+
+
+
+btn.addEventListener("click" , function e() {
+    bg.classList.toggle('bg-dark')
+})
+
